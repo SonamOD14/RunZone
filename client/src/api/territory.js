7fcd4293dfdefb9ac8499c050bf87aae272045ca
SonamOD14 <wangdisonam123456789@gmail.com>
2026-06-15 17:07:21 +0545

    client/src/api/territory.js

--- client/src/api/territory.js
+++ client/src/api/territory.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
+import { API_BASE_URL } from './config'
 
 const API = axios.create({
-  baseURL: '/api'
+  baseURL: API_BASE_URL
 })
 
-// Attach JWT token to every request automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
@@ -13,7 +13,6 @@ API.interceptors.request.use((config) => {
   return config
 })
 
-// Territory API calls
 export const getAllTiles = () => API.get('/territory/all')
 export const getMyTiles = () => API.get('/territory/mine')
 export const getUserTiles = (id) => API.get(`/territory/user/${id}`)
