8016144c4a1d316d1703ae07ffb6e6e7a1b42660
SonamOD14 <wangdisonam123456789@gmail.com>
2026-07-13 23:23:19 +0545

    server/server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const app = require('./src/app');
+const DecayService = require('./src/services/decayService');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
+
+DecayService.startDecaySchedule();
 
 app.listen(PORT, () => {
   console.log(`🚀 RunZone server running on port ${PORT}`);
