06f48cf94498c52026d43f32c70cd8228010b76b
SonamOD14 <wangdisonam123456789@gmail.com>
2026-07-14 09:59:53 +0545

    server/src/routes/territory.routes.js

--- server/src/routes/territory.routes.js
+++ server/src/routes/territory.routes.js
@@ -8,7 +8,9 @@ router.get('/all', TerritoryController.getAllTiles);
 
 // Protected routes - must be logged in
 router.get('/mine', verifyToken, TerritoryController.getMyTiles);
+router.get('/history', verifyToken, TerritoryController.getMyHistory);
 router.get('/user/:id', verifyToken, TerritoryController.getUserTiles);
 router.get('/tile', verifyToken, TerritoryController.getTileInfo);
+router.delete('/abandon', verifyToken, TerritoryController.abandonTile);
 
 module.exports = router;
